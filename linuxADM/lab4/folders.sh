fada01ac62b5fdf77a824aa8c4a3410d34bdf9ef
nawm77 <alpha050603@gmail.com>
2024-03-07 21:45:08 +0300

    end lab4

--- linuxADM/lab4/folders.sh
+++ linuxADM/lab4/folders.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-mkdir -p /shared/projects
-cd /shared/projects
+mkdir -p /home/vagrant/shared/project
+echo "Created dir /home/vagrant/shared/project"
+cd /home/vagrant/shared/project
 mkdir -p stage{1..3}/documents stage{1..3}/budget && \
 for i in {1..3}; do echo "STAGE $i" > stage$i/documents/document.txt; echo "BUDGET FOR STAGE $i" > stage$i/budget/readme.txt; done
