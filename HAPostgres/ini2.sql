afd903565d87f69d48bb9012743e885c5329a1f3
i.s.chaynichkov <alpha050603@gmail.com>
2024-04-30 00:34:46 +0300

    add pgpool compose, pg configs

--- HAPostgres/ini2.sql
+++ HAPostgres/ini2.sql
@@ -1,81 +1,79 @@
 CREATE TABLE IF NOT EXISTS "Locations"
 (
-    "location_id" integer NOT NULL,
-    "country"     text,
-    "city"        text,
-    "home_number" text,
-    PRIMARY KEY ("location_id")
+    "location_id" SERIAL PRIMARY KEY,
+    "country"     TEXT,
+    "city"        TEXT,
+    "home_number" TEXT
 );
 
 CREATE TABLE IF NOT EXISTS "Devices"
 (
-    "device_id"   integer NOT NULL,
-    "device_name" text,
-    "device_type" text,
-    "location_id" bigint,
-    "status"      text,
-    "last_update" timestamp,
-    PRIMARY KEY ("device_id")
+    "device_id" SERIAL PRIMARY KEY,
+    "device_name" TEXT,
+    "device_type" TEXT,
+    "location_id" BIGINT,
+    "status"      TEXT,
+    "last_update" TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS "Sensors"
 (
-    "sensor_id"   integer NOT NULL,
-    "device_id"   bigint,
-    "sensor_type" text,
-    "value"       double precision,
-    "timestamp"   timestamp,
-    PRIMARY KEY ("sensor_id")
+    "sensor_id" SERIAL PRIMARY KEY,
+    "device_id" BIGINT,
+    "sensor_type" TEXT,
+    "value" DOUBLE PRECISION,
+    "timestamp" TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS "SecuritySystems"
 (
-    "security_id"   integer NOT NULL,
-    "device_id"     bigint,
-    "security_type" text,
-    "status"        text,
-    "timestamp"     timestamp,
-    PRIMARY KEY ("security_id")
+    "security_id" SERIAL PRIMARY KEY,
+    "device_id" BIGINT,
+    "security_type" TEXT,
+    "status" TEXT,
+    "timestamp" TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS "TrafficControl"
 (
-    "traffic_id"           integer NOT NULL,
-    "device_id"            bigint,
-    "traffic_light_status" text,
-    "traffic_flow"         double precision,
-    "timestamp"            timestamp,
-    PRIMARY KEY ("traffic_id")
+    "traffic_id" SERIAL PRIMARY KEY,
+    "device_id" BIGINT,
+    "traffic_light_status" TEXT,
+    "traffic_flow" DOUBLE PRECISION,
+    "timestamp" TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS "PowerNetworks"
 (
-    "power_id"          integer NOT NULL,
-    "device_id"         bigint,
-    "power_consumption" double precision,
-    "renewable_energy"  text,
-    "timestamp"         timestamp,
-    PRIMARY KEY ("power_id")
+    "power_id" SERIAL PRIMARY KEY,
+    "device_id" BIGINT,
+    "power_consumption" DOUBLE PRECISION,
+    "renewable_energy" TEXT,
+    "timestamp" TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS "DataCollection"
 (
-    "data_id"      integer NOT NULL,
-    "device_id"    bigint,
-    "data_type"    text,
-    "data_content" text
+    "data_id" SERIAL PRIMARY KEY,
+    "device_id" BIGINT,
+    "data_type" TEXT,
+    "data_content" TEXT
 );
-
 
 ALTER TABLE "Devices"
     ADD CONSTRAINT "Devices_fk3" FOREIGN KEY ("location_id") REFERENCES "Locations" ("location_id");
+
 ALTER TABLE "Sensors"
     ADD CONSTRAINT "Sensors_fk1" FOREIGN KEY ("device_id") REFERENCES "Devices" ("device_id");
+
 ALTER TABLE "SecuritySystems"
     ADD CONSTRAINT "SecuritySystems_fk1" FOREIGN KEY ("device_id") REFERENCES "Devices" ("device_id");
+
 ALTER TABLE "TrafficControl"
     ADD CONSTRAINT "TrafficControl_fk1" FOREIGN KEY ("device_id") REFERENCES "Devices" ("device_id");
+
 ALTER TABLE "PowerNetworks"
     ADD CONSTRAINT "PowerNetworks_fk1" FOREIGN KEY ("device_id") REFERENCES "Devices" ("device_id");
+
 ALTER TABLE "DataCollection"
     ADD CONSTRAINT "DataCollection_fk1" FOREIGN KEY ("device_id") REFERENCES "Devices" ("device_id");
